0bef35332878b4b726f18c4a3768d1a7d46aaa70
mega123-art <agrawalparth326@gmail.com>
2025-10-21 11:28:19 +0000

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	renamed:    q4-escrow/.gitignore -> Q4_escrow/.gitignore
    	renamed:    q4-escrow/.prettierignore -> Q4_escrow/.prettierignore
    	renamed:    q4-escrow/Anchor.toml -> Q4_escrow/Anchor.toml
    	renamed:    q4-escrow/Cargo.toml -> Q4_escrow/Cargo.toml
    	renamed:    q4-escrow/migrations/deploy.ts -> Q4_escrow/migrations/deploy.ts
    	renamed:    q4-escrow/package.json -> Q4_escrow/package.json
    	renamed:    q4-escrow/programs/q4-escrow/Cargo.toml -> Q4_escrow/programs/q4-escrow/Cargo.toml
    	renamed:    q4-escrow/programs/q4-escrow/src/instructions/make.rs -> Q4_escrow/programs/q4-escrow/src/instructions/make.rs
    	renamed:    q4-escrow/programs/q4-escrow/src/instructions/mod.rs -> Q4_escrow/programs/q4-escrow/src/instructions/mod.rs
    	new file:   Q4_escrow/programs/q4-escrow/src/instructions/refund.rs
    	new file:   Q4_escrow/programs/q4-escrow/src/instructions/take.rs
    	renamed:    q4-escrow/programs/q4-escrow/src/lib.rs -> Q4_escrow/programs/q4-escrow/src/lib.rs
    	renamed:    q4-escrow/programs/q4-escrow/src/state/mod.rs -> Q4_escrow/programs/q4-escrow/src/state/mod.rs
    	renamed:    q4-escrow/rust-toolchain.toml -> Q4_escrow/rust-toolchain.toml
    	renamed:    q4-escrow/tests/q4-escrow.ts -> Q4_escrow/tests/q4-escrow.ts
    	renamed:    q4-escrow/tsconfig.json -> Q4_escrow/tsconfig.json
    	renamed:    q4-escrow/yarn.lock -> Q4_escrow/yarn.lock
    	deleted:    q4-escrow/programs/q4-escrow/src/instructions/refund.rs
    	deleted:    q4-escrow/programs/q4-escrow/src/instructions/take.rs

---
+++ Q4_escrow/programs/q4-escrow/src/instructions/take.rs
@@ -0,0 +1,19 @@
+use anchor_lang::prelude::*;
+
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{
+        close_account, transfer_checked, CloseAccount, Mint, TokenAccount, TokenInterface,
+        TransferChecked,
+    },
+};
+
+use crate::Escrow;
+
+#[derive(Accounts)]
+pub struct Take<'info> {
+}
+
+impl<'info> Take<'info> {
+     Includes Deposit, Withdraw and Close Vault
+}
