c5d65c187a0785f39a1d041f8b06c0165dd9b381
mega123-art <agrawalparth326@gmail.com>
2025-10-21 11:10:39 +0000

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   q4-escrow/programs/q4-escrow/Cargo.toml
    	new file:   q4-escrow/programs/q4-escrow/src/instructions/make.rs
    	new file:   q4-escrow/programs/q4-escrow/src/instructions/mod.rs
    	new file:   q4-escrow/programs/q4-escrow/src/instructions/refund.rs
    	new file:   q4-escrow/programs/q4-escrow/src/instructions/take.rs
    	modified:   q4-escrow/programs/q4-escrow/src/lib.rs
    	new file:   q4-escrow/programs/q4-escrow/src/state/mod.rs

--- q4-escrow/programs/q4-escrow/src/lib.rs
+++ q4-escrow/programs/q4-escrow/src/lib.rs
@@ -1,16 +1,27 @@
 use anchor_lang::prelude::*;
+pub mod instructions;
+pub mod state;
 
+pub use instructions::*;
+pub use state::*;
 declare_id!("6ymMQ8T2WxJ4ukkeQ78g6KNgH8DgcEJ8zd7RW5MT2kza");
 
 #[program]
 pub mod q4_escrow {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+pub fn make(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        ctx.accounts.deposit(deposit)?;
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)
     }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()
+    }
+
+    // pub fn take(ctx: Context<Take>) -> Result<()> {
+    //     ctx.accounts.deposit()?;
+    //     ctx.accounts.withdraw_and_close_vault()
+    // }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+
