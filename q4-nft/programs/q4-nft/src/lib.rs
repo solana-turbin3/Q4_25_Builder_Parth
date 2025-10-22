f5b0b14ffe831980271f268c1a52af9079f219e3
mega123-art <agrawalparth326@gmail.com>
2025-10-22 19:08:56 +0000

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   q4-nft/Cargo.lock
    	modified:   q4-nft/programs/q4-nft/src/error.rs
    	new file:   q4-nft/programs/q4-nft/src/instructions/create_collection.rs
    	new file:   q4-nft/programs/q4-nft/src/instructions/freeze_nft.rs
    	new file:   q4-nft/programs/q4-nft/src/instructions/mint_nft.rs
    	new file:   q4-nft/programs/q4-nft/src/instructions/mod.rs
    	new file:   q4-nft/programs/q4-nft/src/instructions/thaw_nft.rs
    	new file:   q4-nft/programs/q4-nft/src/instructions/update_nft.rs
    	new file:   q4-nft/programs/q4-nft/src/instructions/whitelist_creator.rs
    	modified:   q4-nft/programs/q4-nft/src/lib.rs
    	new file:   q4-nft/programs/q4-nft/src/state/collection_authority.rs
    	new file:   q4-nft/programs/q4-nft/src/state/mod.rs
    	new file:   q4-nft/programs/q4-nft/src/state/whitelisted_creators.rs

--- q4-nft/programs/q4-nft/src/lib.rs
+++ q4-nft/programs/q4-nft/src/lib.rs
@@ -1,16 +1,36 @@
 use anchor_lang::prelude::*;
+mod instructions;
+mod state;
+mod error;
 
+use instructions::*;
+// use state::*;
 declare_id!("Ck7AXoSAGnYvp7xViWaKakeRnGHB7YV4gHa1qv3PfyQ3");
 
 #[program]
 pub mod q4_nft {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+pub fn whitelist_creator(ctx: Context<WhitelistCreator>) -> Result<()> {
+        ctx.accounts.whitelist_creator()
     }
-}
+    
+    pub fn create_collection(ctx: Context<CreateCollection>, args: CreateCollectionArgs) -> Result<()> {
+        ctx.accounts.create_collection(args, &ctx.bumps)
+    }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+    pub fn mint_nft(ctx: Context<MintNft>) -> Result<()> {
+        ctx.accounts.mint_nft()
+    }
+
+    // pub fn freeze_nft(ctx: Context<FreezeNft>) -> Result<()> {
+    //     ctx.accounts.freeze_nft()
+    // }
+
+    // pub fn thaw_nft(ctx: Context<ThawNft>) -> Result<()> {
+    //     ctx.accounts.thaw_nft()
+    // }
+
+    // pub fn thaw_nft(ctx: Context<UpdateNft>, new_name: String) -> Result<()> {
+    //     ctx.accounts.update_nft(new_name)
+    // }
+}
