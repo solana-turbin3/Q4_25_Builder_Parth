494be43900d6dbec28c93f6750dccbd2f00689f0
mega123-art <agrawalparth326@gmail.com>
2025-10-22 12:00:30 +0000

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   Q4_amm/programs/q4-amm/src/lib.rs
    	deleted:    q4-amm/target/debug/incremental/q4_amm-17abwfl0cwbvk/s-hc9p7f26ro-1h68mgu-working/dep-graph.part.bin
    	deleted:    q4-amm/target/debug/incremental/q4_amm-17abwfl0cwbvk/s-hc9p7f26ro-1h68mgu.lock
    	deleted:    q4-amm/target/debug/incremental/q4_amm-2u2kvgfecodmx/s-hc9p7f29zb-0oggunj-working/dep-graph.part.bin
    	deleted:    q4-amm/target/debug/incremental/q4_amm-2u2kvgfecodmx/s-hc9p7f29zb-0oggunj.lock

--- Q4_amm/programs/q4-amm/src/lib.rs
+++ Q4_amm/programs/q4-amm/src/lib.rs
@@ -22,13 +22,13 @@ pub mod q4_amm {
         ctx.accounts.deposit(amount, max_x, max_y)
     }
 
-    pub fn withdraw(ctx: Context<Withdraw>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
-        ctx.accounts.withdraw(amount, max_x, max_y)
-    }
+    // pub fn withdraw(ctx: Context<Withdraw>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+    //     ctx.accounts.withdraw(amount, max_x, max_y)
+    // }
 
-    pub fn swap(ctx: Context<Swap>, is_x: bool, amount_in: u64, min_amount_out: u64) -> Result<()> {
-        ctx.accounts.swap(is_x, amount_in, min_amount_out)
-    }
+    // pub fn swap(ctx: Context<Swap>, is_x: bool, amount_in: u64, min_amount_out: u64) -> Result<()> {
+    //     ctx.accounts.swap(is_x, amount_in, min_amount_out)
+    // }
     
 }
 
